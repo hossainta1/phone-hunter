a26dc4a47d3fb721714f5ca7b4ea6f4564ba1bca
hossainta1 <tanvir.hossain303030@gmail.com>
2023-06-24 01:19:21 +0600

    Display search result of phone

--- js/app.js
+++ js/app.js
@@ -11,6 +11,24 @@ const displayPhones = phones => {
     const phoneContainer = document.getElementById('phones-container');
     phoneContainer.textContent = '';
 
+    // Display 20 phone only
+
+    phones = phones.slice(0, 10);
+
+
+    // Display no  phones found  
+
+    const noPhone = document.getElementById('no-phone-massage');
+
+    if (phones.length === 0) {
+        noPhone.classList.remove('d-none');
+    }
+    else {
+        noPhone.classList.add('d-none');
+    }
+
+    // Display all phone 
+
     phones.forEach((phone) => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -38,4 +56,4 @@ document.getElementById('btn-search').addEventListener('click', function () {
     loadPhone(searchText);
 })
 
-loadPhone();
+// loadPhone();
