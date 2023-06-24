4714cd38e3faf3e7eb61b28ebd676c07e0af7d54
hossainta1 <tanvir.hossain303030@gmail.com>
2023-06-24 12:33:35 +0600

    Show phone details data on Modal

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ const displayPhones = (phones, dataLimit) => {
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural
                                 lead-in to additional content. This content is a little bit longer.</p>
-                                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" >Show Details</button>
+                                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                         </div>
                     </div>
        
@@ -114,7 +114,23 @@ const loadPhoneDetails = async id => {
 
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data)
+    displayPhoneDetails(data.data)
+}
+
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+
+    const phoneDatils = document.getElementById('phone-details');
+    phoneDatils.innerHTML = `
+   <p>Release Date : ${phone.releaseDate ? phone.releaseDate : 'No Releasedate Found'}</p>
+   <P>Storage :${phone.mainFeatures ? phone.mainFeatures.storage : 'Storage ingormation not found'}</P>
+   <P>Others :${phone.others ? phone.others.Bluetooth : 'No bluetooth information'}</P>
+   
+   `
 }
 
 
